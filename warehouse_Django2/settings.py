156e7ed24ebea89e7a7c081fb3680d2dbccac2e6
Daniel <angry871015@gmail.com>
2024-10-16 16:34:19 +0800

    20241016_v2

--- warehouse_Django2/settings.py
+++ warehouse_Django2/settings.py
@@ -92,17 +92,11 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+        'OPTIONS': {
+            'min_length': 8,  # 你可以降低這個值，或者完全刪除這個驗證器
+        }
+    }
 ]
 
 
